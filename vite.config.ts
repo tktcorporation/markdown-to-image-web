3a24f3d539a6db2ee37bcd373cd1218c42075d24
tktcorporation <tktcorporation.go@gmail.com>
2024-11-28 16:42:57 +0000

    chore: devcontainer のための設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-})
+  server: {
+    host: command === 'serve' ? '0.0.0.0' : 'localhost'
+  }
+}))
